b65d0f7e1d4ac82f2c3f84deb9c9bd65a407f9fe
chengdenghui <1017096321@qq.com>
2017-11-03 10:41:01 +0800

    提交

--- GrownUpApp-Swift/ViewController.swift
+++ GrownUpApp-Swift/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("大家好! 现在开始学习Swift啦...")
+        
     }
 
     override func didReceiveMemoryWarning() {
