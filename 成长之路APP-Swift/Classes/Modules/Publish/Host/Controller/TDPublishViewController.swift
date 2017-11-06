336f2d36540a8f9170d80b41d086ea8c3b48fce1
chengdenghui <1017096321@qq.com>
2017-11-06 11:17:33 +0800

    添加tabBar资源plist文件

---
+++ 成长之路APP-Swift/Classes/Modules/Publish/Host/Controller/TDPublishViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TDPublishViewController.swift
+//  成长之路APP-Swift
+//
+//  Created by mac on 2017/11/6.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+import UIKit
+
+class TDPublishViewController: TDBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
