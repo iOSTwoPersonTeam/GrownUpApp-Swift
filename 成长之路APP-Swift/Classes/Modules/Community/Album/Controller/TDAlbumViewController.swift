2781201932c2019453093316c80cc5fd9aafcc20
chengdenghui <1017096321@qq.com>
2017-11-12 11:19:12 +0800

    搭建下载中框架

---
+++ 成长之路APP-Swift/Classes/Modules/Community/Album/Controller/TDAlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TDAlbumViewController.swift
+//  成长之路APP-Swift
+//
+//  Created by mac on 2017/11/12.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+import UIKit
+
+class TDAlbumViewController: TDBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
